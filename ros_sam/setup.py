4bda5d5d756886c206e2fdb78876b1f106308fd5
mhubi <m.huber_1994@hotmail.de>
2023-11-15 19:05:02 +0000

    formatted code, refer https://github.com/psf/black

--- ros_sam/setup.py
+++ ros_sam/setup.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Try catkin install
-from catkin_pkg.python_setup import generate_distutils_setup
 from distutils.core import setup
-
 from pathlib import Path
 
+from catkin_pkg.python_setup import generate_distutils_setup
 
-with open(f'{Path(__file__).parent}/requirements.txt', 'r') as f:
-   pip_dependencies = f.readlines()
+with open(f"{Path(__file__).parent}/requirements.txt", "r") as f:
+    pip_dependencies = f.readlines()
 
 
-d = generate_distutils_setup(
-   packages=['ros_sam'],
-   package_dir={'': 'src'}
-)
+d = generate_distutils_setup(packages=["ros_sam"], package_dir={"": "src"})
 
-d.update({'install_requires': pip_dependencies})
+d.update({"install_requires": pip_dependencies})
 
 setup(**d)
--- ros_sam/src/ros_sam/sam.py
+++ ros_sam/src/ros_sam/sam.py
@@ -1,41 +1,43 @@
-from pathlib import Path
-import torch
-
-from segment_anything import sam_model_registry, \
-                             SamPredictor
-
 # Free up GPU memory before loading the model
 import gc
+from pathlib import Path
+
+import torch
+from segment_anything import SamPredictor, sam_model_registry
 
 
-class SAM():
+class SAM:
     def __init__(self, model_type, cuda_device=None) -> None:
-        model_dir   = Path(Path(__file__).parent.parent.parent / 'models').absolute()
-        checkpoints = list(model_dir.glob(f'sam_{model_type}_*.pth'))
+        model_dir = Path(Path(__file__).parent.parent.parent / "models").absolute()
+        checkpoints = list(model_dir.glob(f"sam_{model_type}_*.pth"))
 
         if len(checkpoints) == 0:
-            raise RuntimeError(f'No matching checkpoints for SAM model "{model_type}" was found in "{model_dir}"')
-        
+            raise RuntimeError(
+                f'No matching checkpoints for SAM model "{model_type}" was found in "{model_dir}"'
+            )
+
         if len(checkpoints) > 1:
-            raise RuntimeError(f'No unique checkpoint for SAM model "{model_type}" found in "{model_dir}"')
-        
+            raise RuntimeError(
+                f'No unique checkpoint for SAM model "{model_type}" found in "{model_dir}"'
+            )
+
         gc.collect()
         torch.cuda.empty_cache()
 
-        sam_model    = sam_model_registry[model_type](checkpoint=checkpoints[0])
+        sam_model = sam_model_registry[model_type](checkpoint=checkpoints[0])
         self._device = cuda_device
 
         if self._device is not None:
             sam_model.to(device=self._device)
 
         self._predictor = SamPredictor(sam_model)
-    
+
     def __del__(self):
         gc.collect()
         torch.cuda.empty_cache()
 
     def segment(self, img, points, point_labels, boxes=None, multimask=True):
-        self._predictor.set_image(img)        
+        self._predictor.set_image(img)
         return self._predictor.predict(
             point_coords=points,
             point_labels=point_labels,
