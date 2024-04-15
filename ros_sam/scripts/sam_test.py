fc3c8e84f7ed9ffa958a4151977724cdde96b73d
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2024-04-15 12:04:16 +0100

    Separated message and implementation package.

--- ros_sam/scripts/sam_test.py
+++ ros_sam/scripts/sam_test.py
@@ -9,8 +9,8 @@ from pathlib   import Path
 
 from geometry_msgs.msg import Point as PointMsg
 from std_msgs.msg      import Int32MultiArray as Int32MultiArrayMsg
-from ros_sam.srv import Segmentation         as SegmentationSrv, \
-                        SegmentationRequest  as SegmentationRequestMsg
+from ros_sam_msgs.srv  import Segmentation         as SegmentationSrv, \
+                              SegmentationRequest  as SegmentationRequestMsg
 
 
 def show_mask(mask, ax, random_color=False):
@@ -21,17 +21,17 @@ def show_mask(mask, ax, random_color=False):
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
 def show_points(coords, labels, ax, marker_size=375):
     pos_points = coords[labels==1]
     neg_points = coords[labels==0]
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))   
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
 
 
 if __name__ == '__main__':
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     bridge  = CvBridge()
     img_msg = bridge.cv2_to_imgmsg(image)
     boxes = Int32MultiArrayMsg()
-    
+
     points  = np.array([[566, 414]])
 
     msg_points  = [PointMsg(x=x, y=y, z=0) for (x, y) in points]
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     print(img_msg.encoding)
 
     try:
-        res = sam_service(img_msg, msg_points, labels, 
+        res = sam_service(img_msg, msg_points, labels,
                           Int32MultiArrayMsg(), True, True)
     except rospy.ServiceException as e:
         print(f'{e}')
